08a28043fe392fe992f6bf40405bc3d14cc5ff7d
denku <denku@DESKTOP-5TQ0358>
2023-10-18 06:00:46 +0500

    Добавьте файлы проекта.

---
+++ АвторизацияБД/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+
+namespace АвторизацияБД
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static int counter = 0;
+        static int remainedtry = 4;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (tb_login.Text.Count()==0  )
+            {
+                lb_taskbar.Text = ("Введите логин!");
+                return;
+            }
+            else if  (tb_pass.Password.Count() == 0)
+            {
+                lb_taskbar.Text = ("Введите пароль!");
+                return;
+            }
+            var context = new avtorizationEntities();
+            var name = context.User.FirstOrDefault(i => i.login == tb_login.Text && i.pass == tb_pass.Password);
+            if (name != null)
+            {
+                lb_taskbar.Text = ("Успешная авторизация!");
+                Successful_enter successful_wind = new Successful_enter();
+                successful_wind.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+                successful_wind.Show();
+            }
+            else
+            {
+                remainedtry--;
+                if (remainedtry >= 2)
+                {
+                    lb_taskbar.Text = ($"Неверные данные!\nОсталось {remainedtry} попытки.");
+                }
+                else
+                {
+                    lb_taskbar.Text = ($"Неверные данные!\nОсталась {remainedtry} попытка!");
+                }
+                if (counter == 3)
+                {
+                    lb_taskbar.Text = ("Слишком много неверных попыток!\nАвторизация заблокирована!");
+                    tb_login.IsEnabled = false;
+                    tb_pass.IsEnabled = false;
+                    but_enter.IsEnabled = false;
+                }
+                counter++;
+                
+                return;
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Registration registwin = new Registration(null);
+            registwin.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            Hide();
+            registwin.Show();
+        }
+    }
+}
---
+++ АвторизацияБД/Registration.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace АвторизацияБД
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        private User _currentUser = new User();
+        avtorizationEntities database = new avtorizationEntities();
+        public Registration(User selectedUser)
+        {
+            InitializeComponent();
+
+            if (selectedUser != null)
+                _currentUser = selectedUser;
+
+            DataContext = _currentUser;
+        }
+
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentUser.mail))
+                errors.AppendLine("Укажите почту");
+            if (string.IsNullOrWhiteSpace(_currentUser.login))
+                errors.AppendLine("Укажите логин");
+            if (string.IsNullOrWhiteSpace(_currentUser.pass))
+                errors.AppendLine("Укажите пароль");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (database.User.Any(u => u.login == tb_reglogin.Text))
+            {
+                MessageBox.Show("Данный логин уже используется");
+            }
+            else
+            {
+                _currentUser.idType = 1;
+
+                if (_currentUser.id == 0)
+                    avtorizationEntities.GetContext().User.Add(_currentUser);
+
+                try
+                {
+                    avtorizationEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Вы успешно зарегистрировались!");
+
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+                    Hide();
+                    mainWindow.Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }   
+        }
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            Hide();
+            mainWindow.Show();
+        }
+    }
+}
